6ead2d07ecd2d8f63c57b500d1a2b8175f325617
Arnelle Balane <arnellebalane@gmail.com>
2018-10-18 13:24:46 +0800

    Refactor implementation for better master/slave detection

--- master-tab.js
+++ master-tab.js
@@ -7,79 +7,64 @@ const INSTANCE_ID = MasterTab.INSTANCE_ID
     || Math.floor(Math.random() * 0x10000).toString(16);
 
 const state = {
-    ACTIVE: 'a',
-    PASSIVE: 'p',
-    HIDDEN: 'h'
+    ACTIVE: 0,
+    PASSIVE: 1,
+    HIDDEN: 2
 };
 
-const getState = () => {
+const getTabState = () => {
     if (document.visibilityState === 'hidden') return state.HIDDEN;
     if (document.hasFocus()) return state.ACTIVE;
     return state.PASSIVE;
 };
 
-const findLastTabWithState = (tabStack, state) => {
-    for (let i = tabStack.length - 1; i >= 0; i--) {
-        const [instanceId, instanceState] = tabStack[i].split(':');
-        if (instanceState === state) {
-            return instanceId;
-        }
-    }
-    return null;
+const getTabStack = () => {
+    const tabStack = localStorage.getItem(STORAGE_KEY);
+    return tabStack ? tabStack.split(';') : [];
 };
 
-const handleTabStackChange = tabStackString => {
-    const tabStack = tabStackString ? tabStackString.split(';') : [];
-    const tabStates = [state.ACTIVE, state.PASSIVE, state.HIDDEN];
-
-    for (const tabState of tabStates) {
-        const selectedTab = findLastTabWithState(tabStack, tabState);
-        if (selectedTab) {
-            return MasterTab.emit('change', selectedTab === INSTANCE_ID);
-        }
-    }
+const setTabStack = tabStack => {
+    localStorage.setItem(STORAGE_KEY, tabStack.join(';'));
 };
 
-['pageshow', 'pagehide', 'focus', 'blur', 'visibilitychange'].forEach(event => {
-    window.addEventListener(event, () => {
-        const tabStackString = localStorage.getItem(STORAGE_KEY);
-        const tabStack = tabStackString ? tabStackString.split(';') : [];
+const handleTabStackChange = tabStack => {
+    MasterTab.emit('change', tabStack[0] === INSTANCE_ID);
+};
 
-        let index = tabStack.findIndex(tab => tab.split(':')[0] === INSTANCE_ID);
+['pageshow', 'focus', 'visibilitychange'].forEach(event => {
+    window.addEventListener(event, e => {
+        if (getTabState() === state.HIDDEN) {
+            return;
+        }
+
+        const tabStack = getTabStack();
+
+        let index = tabStack.indexOf(INSTANCE_ID);
         if (index < 0) {
             index = tabStack.length;
         }
 
-        const currentState = getState();
-        tabStack[index] = [INSTANCE_ID, currentState].join(':');
+        tabStack.splice(index, 1);
+        tabStack.unshift(INSTANCE_ID);
 
-        if (currentState === state.ACTIVE) {
-            tabStack.push(tabStack[index]);
-            tabStack.splice(index, 1);
-        }
-
-        const updatedTabStackString = tabStack.join(';');
-        localStorage.setItem(STORAGE_KEY, updatedTabStackString);
-        handleTabStackChange(updatedTabStackString);
+        setTabStack(tabStack);
+        handleTabStackChange(tabStack);
     });
 });
 
 window.addEventListener('unload', () => {
-    const tabStackString = localStorage.getItem(STORAGE_KEY);
-    const tabStack = tabStackString ? tabStackString.split(';') : [];
-    const index = tabStack.findIndex(tab => tab.split(':')[0] === INSTANCE_ID);
+    const tabStack = getTabStack();
+    const index = tabStack.indexOf(INSTANCE_ID);
 
-    if (index >= 0) {
-        tabStack.splice(index, 1);
-    }
+    tabStack.splice(index, 1);
 
-    const updatedTabStackString = tabStack.join(';');
-    localStorage.setItem(STORAGE_KEY, updatedTabStackString);
+    setTabStack(tabStack);
+    handleTabStackChange(tabStack);
 });
 
 window.addEventListener('storage', e => {
     if (e.key === STORAGE_KEY) {
-        handleTabStackChange(e.newValue);
+        handleTabStackChange(getTabStack());
     }
 });
 
